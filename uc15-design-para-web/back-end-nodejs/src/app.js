071c857d4c5b0da6b2eaa505415f05efcde9d43b
viniciusfariac <vfariacerqueira@gmail.com>
2025-04-07 12:55:17 -0300

    add = add /seria-a

--- uc15-design-para-web/back-end-nodejs/src/app.js
+++ uc15-design-para-web/back-end-nodejs/src/app.js
@@ -5,4 +5,7 @@ app.get("/", (req, res) => {
   res.send("Hello World, this time youre gonna make this");
 });
 
+app.get("/serie-a", (req, res)=> {
+    res.send("<h2>Serie A Brasileirão</h2> <p> Santos primeiro lugar </p>")
+})
 export default app;
